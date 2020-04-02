ed256512728c5c25fe571e31bc809ad2859e36f7
DREVET <vincent.drevet@thuasne.fr>
2020-04-02 12:14:06 +0200

    Ajout de la goroutine pour obtenir le pourcentage d'avancement du téléchargement

--- cpm.go
+++ cpm.go
@@ -5,7 +5,9 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/shirou/gopsutil/host"
 	"gopkg.in/ini.v1"
@@ -15,10 +17,10 @@ const inipath = "cpm.conf"
 
 // Configuration correspondant au information chargé depuis le fichier de configuration
 type Configuration struct {
-	baseURL    string
-	version    string
-	collection []string
-	localCache string
+	baseURL     string
+	version     string
+	collections []string
+	localCache  string
 }
 
 func LoadSettings(conffilepath string) Configuration {
@@ -32,21 +34,24 @@ func LoadSettings(conffilepath string) Configuration {
 	loadConfiguration.version = config_file.Section("Repo").Key("version").String()
 
 	var parseCollections []string = strings.Split(config_file.Section("Repo").Key("collections").String(), " ")
-	loadConfiguration.collection = parseCollections
+	loadConfiguration.collections = parseCollections
 
 	loadConfiguration.localCache = config_file.Section("Main").Key("local_cache").String()
 	return loadConfiguration
 
 }
 
-func DownloadFile(url string, destdir string) error {
+func DownloadFile(url string, destdir string) {
 
 	var parseURL []string = strings.Split(url, "/")
+
+	done := make(chan int64)
 
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("Erreur lors du téléchargement du fichier : %v", err)
 	}
+	fmt.Println(resp.Header.Get("Content-Length"))
 	defer resp.Body.Close()
 
 	file, errorfile := os.Create(destdir + parseURL[len(parseURL)-1])
@@ -55,9 +60,19 @@ func DownloadFile(url string, destdir string) error {
 	}
 	defer file.Close()
 
-	_, errorwrite := io.Copy(file, resp.Body)
+	totalsize, errconvert := strconv.Atoi(resp.Header.Get("Content-Length"))
+	if errconvert != nil {
+		fmt.Printf("Erreur lors du cast: %v", totalsize)
+	}
+	go PrintProgress(done, totalsize, destdir+parseURL[len(parseURL)-1])
 
-	return errorwrite
+	bytewrite, errorwrite := io.Copy(file, resp.Body)
+	if errorwrite != nil {
+		fmt.Printf("Erreur lors de l'écriture du fichier : %v", errorwrite)
+	}
+
+	done <- bytewrite
+
 }
 
 func GetArchitecture() string {
@@ -68,18 +83,63 @@ func GetArchitecture() string {
 	return system.KernelArch
 }
 
+func PrintProgress(channel chan int64, totalsize int, filepath string) {
+	var stop bool = false
+
+	for {
+		select {
+		case <-channel:
+			stop = true
+		default:
+			file, err := os.Open(filepath)
+			if err != nil {
+				fmt.Printf("Erreur lors de l'ouverture du fichier: %v", err)
+			}
+			fi, errorstat := file.Stat()
+			if errorstat != nil {
+				fmt.Printf("Erreur lors de la récupération des statistiques du fichier: %v", err)
+			}
+			currentsize := fi.Size()
+
+			if currentsize == 0 {
+				currentsize = 1
+			}
+
+			var percent float64 = float64(currentsize) / float64(totalsize) * 100
+
+			fmt.Printf("%.0f", percent)
+			fmt.Println("%")
+
+		}
+		if stop {
+			break
+		}
+		time.Sleep(50 * time.Millisecond)
+	}
+}
+
 func main() {
 
 	// Chargement de la configuration
 	var conf Configuration = LoadSettings(inipath)
 
-	fmt.Println(conf)
-
 	// Récupération des arguments
 	args := os.Args
 
+	if len(args) < 2 {
+		fmt.Println("Erreur argument manquant")
+		os.Exit(1)
+	}
+	// Si on met à jour le cache local
 	if args[1] == "update" {
-		fmt.Println(GetArchitecture())
+		// Si l'arch est en 64bit
+		if GetArchitecture() == "x86_64" {
+			for _, collection := range conf.collections {
+				fmt.Println("Téléchargement du manifeste de la collection " + collection + " :")
+				//fmt.Println(conf.baseURL + "dists/" + conf.version + "/" + collection + "/" + "binary-amd64/Packages.gz")
+				DownloadFile(conf.baseURL+"dists/"+conf.version+"/"+collection+"/"+"binary-amd64/Packages.gz", "")
+			}
+		}
 	}
 
 	//DownloadFile("http://ftp.debian.org/debian/dists/buster/main/binary-amd64/Packages.gz", "")
